b9d448640603985bcebe55756af26edb229f5053
cometang <1403029829@qq.com>
2020-03-12 23:24:27 +0800

    完善项目文档

--- commen/tim/user.js
+++ commen/tim/user.js
@@ -14,49 +14,49 @@ const userList = [{
 		user: 'user2',
 		userId: '3',
 		img:'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=366135374,1364401596&fm=26&gp=0.jpg',
-		userSig: 'eJwtzE8LgjAcxvH3smsha27*Ay8xNEpPCnWdbsXPUIZTy6L3nkyPz*cLzxeVWeFMqkcRIg5Ge7tBqm6AO1gejerJFox8Cq1BouhAMSa*74XhWtRbQ68WZ4wRjPGqA7TWAjegxHM3NfBYfucmy-XV7JK24sc0pWM*ncStK8SFN*xc0leV5FzWM--UMfr9ASceMak_-XVqkWAA9iL98_'
+		userSig: 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwsZQweKU7MSCgswUJStDEwMDI3NzM0tLiExqRUFmUSpQ3NTU1MjAwAAiWpKZCxazMDEwMjWwNISakpkONDM5y8e-2M*syqfcIqPMUTvb1SnMNcs4yDDRpTik2DWyzL200D05oDzVsyjfVqkWABWxMCc_'
 	},
 	{
 		user: 'user3',
 		userId: '4',
 		img:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=275868592,2250122918&fm=26&gp=0.jpg',
-		userSig: 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwiZQweKU7MSCgswUJStDEwMDI3NzM0tLiExqRUFmUSpQ3NTU1MjAwAAiWpKZCxYzNzEwM7IwgaotzkwHmlmc4*aUVxJgkhUYEZJonJ5oFpTrG6OvbZrpHBFe6Bri75RbaZBTbujllhVoYKtUCwBATC*g'
+		userSig: 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwiZQweKU7MSCgswUJStDEwMDI3NzM0tLiExqRUFmUSpQ3NTU1MjAwAAiWpKZCxazMDEwMjUyg4oWZ6YDzQzzNvYsD3dPz0qP9Ekqci8xM832LXNJ90wxDkwKywsM8jbPjsjRdoosrIy0VaoFABNEL9E_'
 	},
 	{
 		user: 'user4',
 		userId: '5',
 		img:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2473035870,2692619587&fm=26&gp=0.jpg',
-		userSig: 'eJwtzMEKgkAUheF3udtCbtNcR4U2QQRhGRqBy2DGuE3GMFlo0bsn6vJ8B-4vnNIieBsPCYgAYT5s1ubRcMUD04RPbS-OsYZkIRGFUmEcj49pHXvTOxEJRBy14XowJTEUUUhTha9980yZOUius1Lujl1eLPdys-20vipTn5a3dT572Uh1dG-tCn5-CoIvrQ__'
+		userSig: 'eJwtzMEKgkAUheF3mXXIdfTqjNDCCgMTCnLTcmBm5BbmYGZi9O6Jujzfgf-LyuLq9aZlCeMesM28SZtnR5ZmxhVf*qGcI80SPwTgcRxJuTxmcNSayRGRA8CiHdWziRA4*gGuFaqmZncZpLDv-FzCfd9-MmcOQp2a0d4iJY9Z4dd5M*6CRqTVlv3*F5svpQ__'
 	},
 	{
 		user: 'user5',
 		userId: '6',
 		img:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3293099503,606929711&fm=26&gp=0.jpg',
-		userSig: 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwmZQweKU7MSCgswUJStDEwMDI3NzM0tLiExqRUFmUSpQ3NTU1MjAwAAiWpKZCxYzNzEwM7I0MYSakpkONDPHPdinLDnE0DE71bkwNcu5ODmjzF87LSOoKs27rDAyKdk9yz*nJLzMv9zVVqkWAC2JMQA_'
+		userSig: 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwmZQweKU7MSCgswUJStDEwMDI3NzM0tLiExqRUFmUSpQ3NTU1MjAwAAiWpKZCxazMDEwMjU0NYSakpkONNPSx7Sw0DOlyKcqOTMtwMw42dxS2w9oZJJbVUBigUW4u5OXc3J*eUWYUbatUi0AAvUvMA__'
 	},
 	{
 		user: 'user6',
 		userId: '7',
 		img:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1643922863,2228588017&fm=26&gp=0.jpg',
-		userSig: 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwuZQweKU7MSCgswUJStDEwMDI3NzM0tLiExqRUFmUSpQ3NTU1MjAwAAiWpKZCxYzNzEwM7I0NYWakpkONDPIOMvRMim1sjQ-Rj-K19wrKT*gINWgMDffLDA5J90yLUbfwC3btyIjPaA429NWqRYAfDQwzw__'
+		userSig: 'eJwtzD0PgjAUheH-0tmQ2xtKKYmDAxgjGzLIprSQm-rRIBJT43*XFMbznOT9slNZRZMZWMYwArYJm7R5jNRRYLniS9uLc6RZxmMAlDJRannMx9FgZhdCIAAsOtI9WBoDCp6ItUL93OTnqa-8Uzv*vub21lq-zw*l7xtVpDV6lNjtsNZNcmy37PcHIzwvpA__'
 	},
 	{
-		user: '11111111111',
+		user: 'user7',
 		userId: '8',
 		img:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1179876196,102524513&fm=26&gp=0.jpg',
-		userSig: 'eJwtzFELgjAUhuH-suuw49xcE7qKIYTVaHrXTbBTHEIZJquI-nuiXn7PB**X1ZVLIvasYDwBtpo2eewGutHEmwWf-nENgTwrUgHAlcq1nh98B*pxdCklB4BZB2onUwJyrlW6VOg*NqnU8uzaDKW9rDth9wfTVNGjO1JtnD3h67MzdRp5mTVb9vsDLfEvPA__'
+		userSig: 'eJwtzF0LwiAYhuH-4nGMd07TDTrpYBBGMCoWnQW69dKXbCJi9N8bbofP9cD9Jaf9MfNmIBWhGZBV2qjN22GHieWCo37crEVNqpwBUCHWZTk-JlgczOSccwoAszp8JZMMKM*lWCrYT81r8wySFWrbqbqt78Xl48c22hgRNHNeWH1W4rDjoZEb8vsDGUUv4A__'
 	},
 	{
-		user: '22222222222',
+		user: 'user8',
 		userId: '9',
 		img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3206878631,547916318&fm=26&gp=0.jpg',
-		userSig: 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwpZQweKU7MSCgswUJStDEwMDI3NzM0tLiExqRUFmUSpQ3NTU1MjAwAAiWpKZCxYzNzEwM7K0MIGakpkONDPPNS283MPN3TLIM9mx2DexzDjL06UyvzzULTA3OUY-Jd0o16nIJUbfJC00O9lWqRYAfNUwkg__'
+		userSig: 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwpZQweKU7MSCgswUJStDEwMDI3NzM0tLiExqRUFmUSpQ3NTU1MjAwAAiWpKZCxazMDEwMjU2M4GakpkONLPILNDQ3CLYtCQ3NCq50qDS3aXcySdGPyuyOCg9PMqowNPHuaCgoCrHIrWw3FapFgA*NjBr'
 	},
 	{
-		user: '中介',
+		user: 'user9',
 		userId: '10',
 		img:'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1779444511,689185070&fm=26&gp=0.jpg',
-		userSig: 'eJw1zF0LgjAYhuH-suOQ1*k2JnSQZ5L0KdSpsbVeJB3OiRH992TW4XM9cL9JVZ6jUfckIzQCsgoblW4HvGPg*K9ONbW1qEgWpwBUCC7l8ujJYq9nZ4xRAFh0wGcwkQKnMqG-Cpo56sfi0J3ypC5zMcnr5rbtVHvZHZV-ee5Mg-uuSmVh4OHW5PMFPp0wYQ__'
+		userSig: 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwoYw0eKU7MSCgswUJStDEwMDI3NzM0tLiExqRUFmUSpQ3NTU1MjAwAAiWpKZCxazMDEwMjUytoCakpkONNQv0ihbOzPPI9Q-LTcy39EpszI1PNI9wihGvzzI3z-FsdjLuSg-oiK8stQr0lapFgBoUDC1'
 	},
 ]
 
